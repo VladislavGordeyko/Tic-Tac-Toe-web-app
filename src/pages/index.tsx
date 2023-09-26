0504d21fa3d10c3c64aeb9f79959c236d19ac60d
Vladislav Gordeyko <gordeykovladislav@gmail.com>
2023-09-27 00:55:59 +0300

    added test send message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 // import { Inter } from 'next/font/google'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Board from '@/components/Board'
 import styles from './home.module.scss';
 
 // const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
+  const [message, setMessage] = useState('');
   useEffect(() => {
       console.log(window.Telegram.WebApp.initData);
       console.log('unsafe', window.Telegram.WebApp.initDataUnsafe);
@@ -18,6 +19,27 @@ const Home = () => {
 
      const onSendData = () => {
       window.Telegram.WebApp.sendData(JSON.stringify({data: 'Some data'}));
+     }
+
+     const onSendDataDirectly = () => {
+      const postData = {
+        message
+      };
+      fetch('http://localhost:3000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .catch(error => {
+      console.error("There was an error with the POST request:", error);
+    });
+     }
+
+     const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+        setMessage(e.currentTarget.value);
      }
 
 
@@ -34,6 +56,8 @@ const Home = () => {
       </div>
       <Board />
       <button onClick={onSendData}>Send data</button>
+      <input value={message} onChange={onChange} />
+      <button onClick={onSendDataDirectly}>Send data</button>
     </main>
   )
 }
