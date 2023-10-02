132ae9904acbcb19270f75ce279388b1f47fcbab
Vladislav Gordeyko <gordeykovladislav@gmail.com>
2023-10-02 23:45:52 +0300

    change in username display

--- src/components/GameStatus/index.tsx
+++ src/components/GameStatus/index.tsx
@@ -14,7 +14,7 @@ const GameStatus: React.FC<IPlayersLabel> = ({ players, status, currentMoveClien
       /> */}
       <div className={styles['player-image-container__image']}/>
     </div>   
-    <span className={styles['player-card__name']}>{player.userName || player.fistName}</span>
+    <span className={styles['player-card__name']}>{player.userName ? player.userName : player.fistName }</span>
   </div>;
 
   const emptyPlayerComponent = () => 
