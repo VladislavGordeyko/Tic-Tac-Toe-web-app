a7a5e82a227d56db3340f818870648888493abe9
Vladislav Gordeyko <gordeykovladislav@gmail.com>
2023-10-05 01:57:57 +0300

    player card change

--- src/components/GameStatus/index.tsx
+++ src/components/GameStatus/index.tsx
@@ -1,66 +1,20 @@
 import React from 'react';
 import { IPlayersLabel } from './models';
-import { IPlayer } from '@/entities/game';
 import styles from './gameStatus.module.scss';
-import Image from 'next/image';
+import PlayerCard from './PlayerCard';
+import EmptyCard from './EmptyCard';
 
-const GameStatus: React.FC<IPlayersLabel> = ({ players, status, currentMoveClientId }) => {
-  const playerCardComponent = (player: IPlayer) => {
-    console.log('player score', player.score);
-    return <div className={styles['player-card']}>
-      <div className={`${styles['player-image-container']} ${player.clientId === currentMoveClientId && styles['player-image-container--active']}`}>
-        {player.avatar !== '' ?
-          <>
-            <Image 
-              className={styles['player-image-container__image']} 
-              alt='player-avatar'
-              height={50}
-              width={50}
-              src={player.avatar}
-            />
-            {player.score > 0 && <div className={styles['player-image-container__score']}>
-              {player.score}
-            </div>}
-          </>
-          : <div 
-            className={`${styles['player-image-container']}  ${player.clientId === currentMoveClientId && styles['player-image-container--active']}`}>
-            <div className={`${styles['player-image-container--mock']}`}><Image 
-              className={styles['player-image-container__image-mock']} 
-              alt='player-avatar-mock'
-              height={25}
-              width={25}
-              src='/assets/smile.svg'
-            /> 
-            </div>
-            {player.score > 0 && <div className={styles['player-image-container__score']}>
-              {player.score}
-            </div>}
-          </div> 
-        } 
-        
-      </div>   
-      <span className={styles['player-card__name']}>{player.userName}</span>
-    </div>;
-  }; 
-
-  const emptyPlayerComponent = () => 
-    <div className={styles['player-card']}>
-      <picture>
-        <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f914/512.webp" type="image/webp"/>
-        <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f914/512.gif" alt="🤔" width="32" height="32"/>
-      </picture>
-    </div>;
-
+const GameStatus: React.FC<IPlayersLabel> = ({ players, status }) => {
   return (
     <div className={styles['game-status']}>
       <div className={styles['game-status__text']}>{status}</div>
       <div className={styles['game-status__cards']}>
         {players && <>
-          {playerCardComponent(players[0])}
+          <PlayerCard player={players[0]} /> 
             VS
           {players[1] !== undefined ? 
-            playerCardComponent(players[1]) : 
-            emptyPlayerComponent()
+            <PlayerCard player={players[1]} />  : 
+            <EmptyCard />
           }
         </>}
       </div>  
